26f91b4ddaef1f13219b0ca9e56f0a25e2217127
A Schwartz <di1221@gmail.com>
2015-09-20 18:02:35 -0500

    add code to display markers for each data item listed

--- js/script.js
+++ js/script.js
@@ -1,7 +1,6 @@
-// This example adds a search box to a map, using the Google Place Autocomplete
-// feature. People can enter geographical searches.
 
-    var realtorList = [
+//data to populate list of Asheville realtors
+        realtorList : [
         {
             name: "Town and Mountain Realty",
             address: "261 Asheland Ave",
@@ -38,131 +37,59 @@
            lat: 35.598361,
            lng: -82.545726
         }
-    ];
-/*
-var Realty = function(data) {
-  this.name = ko.observable(data.name);
-  this.address = ko.observable(data.address);
-  this.city = ko.observable(data.city);
-  this.lat = ko.observable(data.lat);
-  this.lng = ko.observable(data.lng);
-}
-  var infowindow;
-  var map;
+    ],
 
-$(document).ready(function () {
+//using knockout.js to bind values
+mapMarker = function (name, address, city, lat, lng) {
+  this.name = ko.observable(name);
+  this.address = ko.observable(address);
+  this.city = ko.observable(city);
+  this.lat = ko.observable(lat);
+  this.lng = ko.observable(lng);
 
-   initMap();
-});
-
-//google maps
-var viewModel = function() {
-    var self = this;
-    self.list = ko.observableArray([]);
-//console.log("realtor = " + realtor);
-    realtorList.forEach(function(realtor) {
-    self.list.push( new Realty(realtor) );
+  //defines how markers are displayed locations for each realtor
+  var marker = new google.maps.Marker({
+      position: new google.maps.LatLng(lat, lng),
+      map: map,
+      draggable: false,
+      //animation:google.maps.Animation.BOUNCE,
+      icon: 'images/mapmarker_green.png'
   });
 
-  self.currentRealtor = ko.observable( self.list()[0]);
-
-    //location: ko.observable("Asheville, NC"),
-    //Locations: ko.observableArray(data.Locations.arrLocations)
-};
-
-var map, infowindow, markers=[];
-//var gmap = "https://maps.googleapis.com/maps/api/place/nearbysearch";
-
-function initMap(){
-    var asheville = {
-        lat: 35.598888,
-        lng: -82.551392
-    };
-
-    map = new google.maps.Map(document.getElementById('map'), {
-        center: asheville,
-        mapTypeId: google.maps.MapTypeId.TERRAIN,
-        zoom: 13
-    });
-
-
-var marker=new google.maps.Marker({
-  position:asheville,
-  });
-
-marker.setMap(map);
 }
 
-ko.applyBindings(viewModel);
-*/
-
-
-// This example adds a search box to a map, using the Google Place Autocomplete
-// feature. People can enter geographical searches.
-
-
-var Realty = function(data) {
-  this.name = ko.observable(data.name);
-  this.address = ko.observable(data.address);
-  this.city = ko.observable(data.city);
-  this.lat = ko.observable(data.lat);
-  this.lng = ko.observable(data.lng);
-}
-
-
-
-//global for map
-var map;
-
-$(document).ready(function () {
-
-   createMap();
-   ko.applyBindings(viewModel);
-});
-
-function MyViewModel() {
-
-    var self = this;
-    self.Lat = ko.observable(35.598888);
-    self.Lng = ko.observable(-82.551392);
-
-    var self = this;
-    self.list = ko.observableArray([]);
-    realtorList.forEach(function(realtor) {
-    self.list.push( new Realty(realtor) );
-
-  }
-)}
-
-    function createMap(){
-    var elevator;
+//creates map for display for the settings below when page is loaded
+function createMap() {
     var myOptions = {
-        zoom: 13,
+        zoom: 11,
         center: new google.maps.LatLng(35.598888, -82.551392),
         mapTypeId: 'terrain'
     };
     map = new google.maps.Map($('#map')[0], myOptions);
 }
 
-ko.bindingHandlers.map = {
+//global for map
+var map;
 
-  init: function (element, valueAccessor, allBindingsAccessor, viewModel) {
-    var position = new google.maps.LatLng(allBindingsAccessor().latitude(), allBindingsAccessor().longitude());
+    loadMap = function(item){
+      var self = this;
+      self.list = ko.observableArray([]);
+        for (var j = 0; j < item.length; j++)
+          self.list.push( new mapMarker( item[j]['name'], item[j]['address'], item[j]['city'], item[j]['lat'], item[j]['lng']));
+  };
 
-    var marker = new google.maps.Marker({
-      map: allBindingsAccessor().map,
-      position: position,
-      animation:google.maps.Animation.BOUNCE,
-      icon: 'images/mapmarker_green.png',
-      title: name
+    vm = function (){
+
+      var self = this;
+      self.list = ko.observableArray([]);
+      realtorList.forEach(function(realtor) {
+        self.list.push( new mapMarker(realtor) );
+          loadMap( realtorList );
     });
+  }
 
-    viewModel._mapMarker = marker;
-  },
-  update: function (element, valueAccessor, allBindingsAccessor, viewModel) {
-    var latlng = new google.maps.LatLng(allBindingsAccessor().latitude(), allBindingsAccessor().longitude());
-    viewModel._mapMarker.setPosition(latlng);
-    }
- };
+$(document).ready(function () {
+    createMap();
+    ko.applyBindings(vm);
+});
 
-var viewModel = new MyViewModel();

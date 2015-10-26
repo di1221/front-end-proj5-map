353b1e7163995d012116fba58b7934b74bea00df
A Schwartz <di1221@gmail.com>
2015-10-26 12:26:53 -0500

    version of code to go over with a coach

--- js/data.js
+++ js/data.js
@@ -1,70 +1,351 @@
 
-
-    var realtorList = [
-        {
-            name: "Town and Mountain Realty",
-            address: "261 Asheland Ave",
-            city: "Asheville, NC 28801",
-            lat: "35.585316",
-            lng: "-82.556695"
-
-        },
-        {
-            name: "Mosaic Realty",
-            address: "2D Wilson Alley",
-            city: "Asheville, NC 28801",
-            lat:  "35.593317",
-            lng:  "-82.550787"
-
-        },
-        {
-            name: "Greybeard Realty",
-            address: "Gashes Creek",
-            city: "",
-            lat: "35.551894",
-            lng: "-82.473189"
-
-        },
-        {
-            name: "Green Mountain Realty",
-            address: "50-74 Distant View Dr",
-            city: "Asheville, NC 28803",
-            lat: "35.510683",
-            lng: "-82.508379"
-        },
-        {
-           name: "Beverly-Hanks Associates",
-           address: "",
-           city: "Asheville, NC",
-           lat: "35.598361",
-           lng: "-82.545726"
-        },
-        {
-            name: "Remax",
-            address: "50-74 Distant View Dr",
-            city: "Asheville, NC 28801",
-            lat: "35.602376",
-            lng: "-82.551802"
-        },
-        {
-            name: "ERA Sunburst Realty",
-            address: "201 E Chestnut St",
-            city: "Asheville, NC 28801",
-            lat: "35.602617",
-            lng: "-82.549087"
-        },
-        {
-            name: "Appalacian Realty",
-            address: "83 Arlington St",
-            city: "Asheville, NC 28801",
-            lat: "35.601507",
-            lng: "-82.543220"
-        },
-        {
-            name: "Asheville Realty & Associates",
-            address: "1314 Parkwood Pl",
-            city: "Asheville, NC 28806",
-            lat: "35.583911",
-            lng: "-82.602772"
-        }
-    ];
+   results = [
+      {
+         "geometry" : {
+            "location" : {
+               "lat" : 35.572052,
+               "lng" : -82.574851
+            }
+         },
+         "icon" : "https://maps.gstatic.com/mapfiles/place_api/icons/generic_business-71.png",
+         "id" : "ae654e4ad440121f9ca4edafa245fba11ca16928",
+         "name" : "Marlow Real Estate & Appraisal Services",
+         "opening_hours" : {
+            "open_now" : false,
+            "weekday_text" : []
+         },
+         "photos" : [
+            {
+               "height" : 900,
+               "html_attributions" : [
+                  "\u003ca href=\"https://maps.google.com/maps/contrib/116801864141602350626\"\u003eMarlow Real Estate &amp; Appraisal Services\u003c/a\u003e"
+               ],
+               "photo_reference" : "CmRdAAAAUQyT8u5P2PK5jSnEREn5I8caWbd4EdkH1aJmi9UVSGL3iTe37PKu2aQm2z025cPIaj2GQ66bFF4RNIwO-jthWTGpMJUp7tBaBV7K3Q3VWVgoeHIpuQ54Vcq0VUp64oslEhAofzH3WrIX7dylnOx1Hmc6GhQ8aXG6AgcK-GHOkXRzQxAmr8lUpg",
+               "width" : 900
+            }
+         ],
+         "place_id" : "ChIJ5zQ41sWMWYgR8FgB1-SJjbQ",
+         "reference" : "CoQBewAAAM7d8ZrvYkUEQaYekmcP1HU0HbvUicnIofKWpCumdQyZEBpY4QE4haIyYUjscLdh32ZpmO-0lwX4YRMvWPz84pbHzhwg8LFrK-pTzgmrv4AKlC5PowwOV4uolG8X2fw2jK_ePWd-mo-xPVVwEhiPSiC9GizdHvXbMhkxZ-sKMcc0EhC60jdIgTqCP--rz4TT7majGhS6sQvedsdZnMj1W9Ry5zyql4-wkw",
+         "scope" : "GOOGLE",
+         "types" : [ "real_estate_agency", "finance", "point_of_interest", "establishment" ],
+         "vicinity" : "67 Frances Street, Asheville"
+      },
+      {
+         "geometry" : {
+            "location" : {
+               "lat" : 35.56807,
+               "lng" : -82.55089599999999
+            }
+         },
+         "icon" : "https://maps.gstatic.com/mapfiles/place_api/icons/generic_business-71.png",
+         "id" : "c41946f41dbb5cf9f1f5cbec6ce39224fc7a4df8",
+         "name" : "Cove Realty Co Inc",
+         "place_id" : "ChIJYws6PzrzWYgRYzCSFdLRvmM",
+         "reference" : "CnRlAAAAq_ZcAKLRFDuas7tWFPqXTFbKoGDRbbgu7GoFqPtWvv1CohM3TjMVSGAxOAVALXunQDafgEGoYQS46_KaFp4XWy2D43b2U7R5-ktQF_x0yK4-R7Ba6V1NnIXZK4_cw48noLO6Wc0ps2glQ35XFb1fyhIQ1qCMASY4bgevFLIMlqg3oxoUuELUEWIQdZAAwZ6JYQjCWvH9vTw",
+         "scope" : "GOOGLE",
+         "types" : [ "real_estate_agency", "point_of_interest", "establishment" ],
+         "vicinity" : "527 McDowell Street, Asheville"
+      },
+      {
+         "geometry" : {
+            "location" : {
+               "lat" : 35.565612,
+               "lng" : -82.544247
+            }
+         },
+         "icon" : "https://maps.gstatic.com/mapfiles/place_api/icons/generic_business-71.png",
+         "id" : "79068e970549740196fa691e2b7533d6f1eab576",
+         "name" : "Mountain Oak Properties, LLC",
+         "opening_hours" : {
+            "open_now" : false,
+            "weekday_text" : []
+         },
+         "photos" : [
+            {
+               "height" : 566,
+               "html_attributions" : [
+                  "\u003ca href=\"https://maps.google.com/maps/contrib/108244933303658357536\"\u003eMountain Oak Properties, LLC\u003c/a\u003e"
+               ],
+               "photo_reference" : "CmRdAAAAN1jrSfStlOHDuIzBCRg8tBAChWsvYPCyivTi7CyJ0TaTMJVF13lvCttexKQDhTsm8smLq_6AIw7-QssKc4dh5VfobP8WElMeLxY9QXzHpuLmeNa0oqSMkzKPJDAXqrKJEhCvjDwMArxYw2IMw6jfw0dYGhQaIxfJagwpWeIrro9gPEZHQiSqiA",
+               "width" : 566
+            }
+         ],
+         "place_id" : "ChIJI8GExzzzWYgR27mmNWANgus",
+         "rating" : 4.9,
+         "reference" : "CnRwAAAApXs705QY1YAcZMVblZxFySaWa0mvIS9ckKBNumYfUEH2dP40j7gfUCq4HQjtGCKQ53fn9TiY5G7MSS5GPBvKwH7X5dU3RaVvCo_VPnx8cZ8-OQumoHaec4j8fle7ulBZ7GUvyrZT1EW8EklTHBJ-MxIQT2Wpp4ap5-aO02B3GkiPbxoUIYrOScr2P3Y5OYJDtOyq8-GZWGU",
+         "scope" : "GOOGLE",
+         "types" : [ "real_estate_agency", "point_of_interest", "establishment" ],
+         "vicinity" : "32 All Souls Crescent #202, Asheville"
+      },
+      {
+         "geometry" : {
+            "location" : {
+               "lat" : 35.577874,
+               "lng" : -82.55437000000001
+            }
+         },
+         "icon" : "https://maps.gstatic.com/mapfiles/place_api/icons/generic_business-71.png",
+         "id" : "b8c774d6195e1a315f72048a194df7efaddf52f8",
+         "name" : "Purcell & Associates Realty",
+         "place_id" : "ChIJCUJut0nzWYgRopxvTM1HJDM",
+         "reference" : "CnRuAAAAlnBn34leg9KNEsQrxra9cL_06FJb4O1ofbX1DZPBklNTT2Gyh8DDcsYe5M-klDFNo9c2efdNDJ5zXteeGdY_Y2iCI0qquGXCawGgE56G9ZqIBlC_sJoCNTqALBVjo3pOIECR8iGA7-9Jkjbx6BVeqBIQ4IYXijqm1bShxR_JzVv5zxoUauxj6O3EE7ZFeXViWwVl7CAcL8M",
+         "scope" : "GOOGLE",
+         "types" : [ "real_estate_agency", "point_of_interest", "establishment" ],
+         "vicinity" : "76 Livingston Street, Asheville"
+      },
+      {
+         "geometry" : {
+            "location" : {
+               "lat" : 35.565516,
+               "lng" : -82.541529
+            }
+         },
+         "icon" : "https://maps.gstatic.com/mapfiles/place_api/icons/generic_business-71.png",
+         "id" : "655b4fe119f584479de7baba751e579e5d0e9b86",
+         "name" : "Carroll John Realtor",
+         "place_id" : "ChIJLUJ0F8n0WYgRMtQxTOjoE1I",
+         "reference" : "CnRnAAAAWZ5cFuzbLfVT-uG6mqxpXMbIKgWJvaSqGKF3Fvgs017MY0z6IBaV2Fth2uiRIHBlUDaLY4en3K2Duk2xpHcFk5ekESNR_HpTMcrbKa2qOsuY3HWIu2DrgO7vmjCPZZw81kjxTQheI6ZyInqxADeT6xIQiI2PWX4fqC6N-valNNW69xoUjl9jom-hAvUjY5QtwE6-tVfeD1c",
+         "scope" : "GOOGLE",
+         "types" : [ "real_estate_agency", "point_of_interest", "establishment" ],
+         "vicinity" : "12 All Souls Crescent, Asheville"
+      },
+      {
+         "geometry" : {
+            "location" : {
+               "lat" : 35.578421,
+               "lng" : -82.581852
+            }
+         },
+         "icon" : "https://maps.gstatic.com/mapfiles/place_api/icons/generic_business-71.png",
+         "id" : "b20494092ff284681d7a52da12a2653a3dff4dd8",
+         "name" : "Iron Creek Properties",
+         "opening_hours" : {
+            "open_now" : false,
+            "weekday_text" : []
+         },
+         "photos" : [
+            {
+               "height" : 870,
+               "html_attributions" : [
+                  "\u003ca href=\"https://maps.google.com/maps/contrib/104660808218980621920\"\u003eIron Creek Properties\u003c/a\u003e"
+               ],
+               "photo_reference" : "CmRdAAAAUTAL0eX9D0MILkdNQTB017SXQQ9i6upsyjt53SPLtpqKxy_450eYo1vC2Jh2wVjjIk_onppNXtQyMWZ57uX5K8YHH8rnWArr4HbBglSDx9ZB0CCTmMDA_CabMf3OnjO3EhCliBgYcHp-vPHxuW-jf2oYGhRs2_oOiPr6HZyfoc7uBb-kZ8wl4g",
+               "width" : 1600
+            }
+         ],
+         "place_id" : "ChIJNV-xw8iMWYgR28Ec64U1DKU",
+         "rating" : 4.7,
+         "reference" : "CnRpAAAA2tMgLjE-Q0maLe1BzZcpmrtIa0y7tE07CRvYhB5MDITbSLZXD554cPATl02gDOZ_iYkQJ_-Ddg69vtUOV0Nbho2VzDQ6_twuqS6wqxxsug68hm0rNYTsVSDLGbH7q_01lMDfy4CxY77CKhPae6PBRBIQ-VjT9qvNoQjJWw-6P29dLxoU8je1pqXfl1t-74-Tz0W5JymnBNk",
+         "scope" : "GOOGLE",
+         "types" : [ "real_estate_agency", "point_of_interest", "establishment" ],
+         "vicinity" : "Asheville"
+      },
+      {
+         "geometry" : {
+            "location" : {
+               "lat" : 35.566958,
+               "lng" : -82.540684
+            }
+         },
+         "icon" : "https://maps.gstatic.com/mapfiles/place_api/icons/generic_business-71.png",
+         "id" : "771aa50880d607f31f4bed037c99048ffd456ac9",
+         "name" : "Randi Beard/ Carolina Mountain Sales",
+         "photos" : [
+            {
+               "height" : 1535,
+               "html_attributions" : [
+                  "\u003ca href=\"https://maps.google.com/maps/contrib/109372628065581394987\"\u003eRandi Beard/ Carolina Mountain Sales\u003c/a\u003e"
+               ],
+               "photo_reference" : "CmRdAAAA2Gr8un1Y7fmqGOns0IpmliNFeMQKrPnlfvqDl-3OFkhxKyiqhFY6650XIsw4aoNxKMJBC8l-9Mk4rw0Y1iMiRFJU4DIv9D_SzFs5ikgx1f-HMrUR6GlAfWL7uBXfBYEKEhC9-Y1bENWK302jBMOpP1LuGhSGUbWtRRmE_N8ZL3R6vTy9ch0ymQ",
+               "width" : 1537
+            }
+         ],
+         "place_id" : "ChIJfcl8ZFHzWYgR55clRNXrJSE",
+         "reference" : "CoQBdwAAAJhoWD5thy-p7WbOstTo_kZRX9cnVqLp-WttEEL7OnOU11wbKwzZsxxTB3l0YW_VCTbQSjPx0oTVfDbfygAcJziFeS0QCb9XnkBrGQHDXAoQLMgZMrum0Z4Xi0QS7Huiyxn1xcQGmPYxt99cmoLEYeXAGBjdRnuE7U5OYfKTxwDwEhD__h35sHN0ZNJ6Qqiju0VwGhTR-c5WwDEDg0adjjyCiyz5Fk1AOQ",
+         "scope" : "GOOGLE",
+         "types" : [ "real_estate_agency", "point_of_interest", "establishment" ],
+         "vicinity" : "10 Brook Street #235, Asheville"
+      },
+      {
+         "geometry" : {
+            "location" : {
+               "lat" : 35.554182,
+               "lng" : -82.53961700000001
+            }
+         },
+         "icon" : "https://maps.gstatic.com/mapfiles/place_api/icons/generic_business-71.png",
+         "id" : "4d531b818f2f6fe7b3de35c79fd4007040f19282",
+         "name" : "Bill Burgin Real Estate",
+         "place_id" : "ChIJNXjD-h_zWYgRHIPGBKSMzzc",
+         "reference" : "CnRqAAAAPu-FEzvpvLCeHKrt-DOXOgV4HCCv1z2aT_IH7C0XcNrRcwQIhwKjK0GuS7t7OAG_wazAVKlTpneeky6_whOq11ZhoW5h__Xk2sPJN4ar4Cokkkp4GdIjUB6aDbnyw5vhqq7zECKyg_j0sLRgYzw7ThIQSUcxljgWrAtLKdZS-qAybxoUR_uXXiws3K6f8JcgomN3WDfwBpw",
+         "scope" : "GOOGLE",
+         "types" : [ "real_estate_agency", "point_of_interest", "establishment" ],
+         "vicinity" : "467 Hendersonville Road, Asheville"
+      },
+      {
+         "geometry" : {
+            "location" : {
+               "lat" : 35.575687,
+               "lng" : -82.58723500000001
+            }
+         },
+         "icon" : "https://maps.gstatic.com/mapfiles/place_api/icons/generic_business-71.png",
+         "id" : "16abeece52825938508af73931a370350a57819c",
+         "name" : "Eco Concepts Realty",
+         "photos" : [
+            {
+               "height" : 480,
+               "html_attributions" : [
+                  "\u003ca href=\"https://maps.google.com/maps/contrib/100912387706677698315\"\u003eEco Concepts Realty\u003c/a\u003e"
+               ],
+               "photo_reference" : "CmRdAAAAkja8RtaNYdc4pqm368BHL3pXH8jWXGgmVsYxF331fnxUatI5jiU2nfA4EpLKxgSCIq6on6gCHkY9eSEL7MdFFGmRoB-RhmlhDo23-t7zU-5NCQxDVhXdyUtuagIsRnrrEhD-IAOmUOFUme_teFhnvoBsGhTtYZghIHSAPmZKLNk51a7JWl2qWA",
+               "width" : 640
+            }
+         ],
+         "place_id" : "ChIJT_QhCuuMWYgRBrNXTeupDOM",
+         "reference" : "CnRnAAAAot8GEdciLYi3YBLptL7TjWRxTBCVGtajhhk4Eqt1wC9ds-dzBz5qLOSLQO-qlNQRAKd-tf2kq5O5Ckq_NGukx4iel-TbadZpa-5AAh2rZZGvxSRPboZgchqN07dlMRLf11Gyv_8vEST2YZiWGg7SPRIQRQhDM23a-Vgu5W0DFg8IjxoUpKVjD5uiTBj4EGURF3_qscfIQRs",
+         "scope" : "GOOGLE",
+         "types" : [ "real_estate_agency", "point_of_interest", "establishment" ],
+         "vicinity" : "10 Sevan Court, Asheville"
+      },
+      {
+         "geometry" : {
+            "location" : {
+               "lat" : 35.585055,
+               "lng" : -82.55672199999999
+            }
+         },
+         "icon" : "https://maps.gstatic.com/mapfiles/place_api/icons/generic_business-71.png",
+         "id" : "ca09cf19e82ef0dbc3759c32eb382b347aece946",
+         "name" : "Town And Mountain Realty",
+         "opening_hours" : {
+            "open_now" : false,
+            "weekday_text" : []
+         },
+         "photos" : [
+            {
+               "height" : 639,
+               "html_attributions" : [
+                  "\u003ca href=\"https://maps.google.com/maps/contrib/111520295362987365883\"\u003eTown And Mountain Realty\u003c/a\u003e"
+               ],
+               "photo_reference" : "CmRdAAAAjZKXS6JorGhtXWtpA0Nm775IgfgRLFe6PV2q2mPJ-cX5fRDHD17yyogD8XAXCxFhfS1EVqrkQxYLGSrqeDNYbb-q8EZx9c3hkC4fxu499qioSv_0d5TXMM9MZsSyWUr5EhCM7BJdQQzXzhFepL30SrasGhQflUvYuBe_bB7qIYsYXBV2HKrp4w",
+               "width" : 960
+            }
+         ],
+         "place_id" : "ChIJa02zeU7zWYgRWQj6hTbHj08",
+         "rating" : 4.3,
+         "reference" : "CnRrAAAAF950g3c-SXjNqgzUJUVEgP3cKuHIwpRcrUIZbZtTCoh4jX67XYys5WIOzmlVd8opil7jDYAw7sQCv2uUNAgtqmaSTD7oVtSEkUy9Br9ILYAP0_6uRT91bL7OL6Yj7SzjhNt25sDD_85LxndJr5meKBIQKTUmwCkGUWmwghHUx5yKBRoUV-ShpLLCuo0-B-oKfr4_c8TSjjc",
+         "scope" : "GOOGLE",
+         "types" : [ "real_estate_agency", "point_of_interest", "establishment" ],
+         "vicinity" : "261 Asheland Avenue #103, Asheville"
+      },
+      {
+         "geometry" : {
+            "location" : {
+               "lat" : 35.564018,
+               "lng" : -82.534139
+            }
+         },
+         "icon" : "https://maps.gstatic.com/mapfiles/place_api/icons/generic_business-71.png",
+         "id" : "5805f11d1fc680aa3a5a5f9da3e70181109b1295",
+         "name" : "Asheville Phoenix Properties",
+         "opening_hours" : {
+            "open_now" : false,
+            "weekday_text" : []
+         },
+         "photos" : [
+            {
+               "height" : 2592,
+               "html_attributions" : [
+                  "\u003ca href=\"https://maps.google.com/maps/contrib/102106012456533959728\"\u003eAsheville Phoenix Properties\u003c/a\u003e"
+               ],
+               "photo_reference" : "CmRdAAAALB8cNi8z02kryqCdD3cAlETl0zuXOrwKBpy85VCAGW4sQg6nerG4UeQUG0ORBo2IrMc-XmA0O3SNVkpEHosMTPM5LITMEOkDr8dT95ggJR6pclPTxCWLK8tuMUa7Z0TwEhDjFTGLhVzx65Pjp2cCw9U-GhTFrAu0E8WNgL2tHqB5f4Af0KxaGA",
+               "width" : 3888
+            }
+         ],
+         "place_id" : "ChIJfcl8ZFHzWYgRBzXR7K3GIjw",
+         "rating" : 3.2,
+         "reference" : "CnRvAAAAZ7z9jZQl9T7bRRx5eJ8p2jcb6sX6i0PPRnyhjszI-ZPlxwve6ooaE8XryHmkL3vndrfVI4kIwgz7HulgpyfZyL8MOqK2DcInzyzYOm88fKrVas0Ju2TpOapIyvbzzzaefgBhHAMk3Hndg_IxoIBMxhIQVb_9Y7gv30H9VL8_KoUZ0hoUvEeAYKYNCquFJQBagpUPtFXHXx0",
+         "scope" : "GOOGLE",
+         "types" : [ "real_estate_agency", "point_of_interest", "establishment" ],
+         "vicinity" : "55 Sweeten Creek Road Suite 101, Asheville"
+      },
+      {
+         "geometry" : {
+            "location" : {
+               "lat" : 35.558061,
+               "lng" : -82.602617
+            }
+         },
+         "icon" : "https://maps.gstatic.com/mapfiles/place_api/icons/generic_business-71.png",
+         "id" : "e2b296d53609b44d6a9f7c93116e4b4741e38eae",
+         "name" : "Bear Creek Realty & Property",
+         "place_id" : "ChIJG3LWxP-MWYgRjJuRd9bv_8o",
+         "reference" : "CnRwAAAAgxfnYCWzU7gznsA9ES7xFeytRQJi3s4s0TKyEi7nfDVp5cgaZSlrhc0UpQXycDjmQSiat0fKHGjl6oCTylM9Y4QQ5OMIp-yoKyiT4KIXMYeCzstFAUoqEkn1CzHUPVbxdJIjwAB5TMqcwdtxYrbkgxIQ9QVS7EhkBFXpB5yi7lCfbxoUHkee3lqVo6p_B6WPGSGyFSTteFc",
+         "scope" : "GOOGLE",
+         "types" : [ "real_estate_agency", "point_of_interest", "establishment" ],
+         "vicinity" : "103 South Bear Creek Road, Asheville"
+      },
+      {
+         "geometry" : {
+            "location" : {
+               "lat" : 35.543821,
+               "lng" : -82.597525
+            }
+         },
+         "icon" : "https://maps.gstatic.com/mapfiles/place_api/icons/generic_business-71.png",
+         "id" : "a22c3cda67fad09ee0975fd8d8232840ff1344b8",
+         "name" : "Re/Max All Stars Realty: Shawn Lynch",
+         "opening_hours" : {
+            "open_now" : false,
+            "weekday_text" : []
+         },
+         "photos" : [
+            {
+               "height" : 2048,
+               "html_attributions" : [
+                  "\u003ca href=\"https://maps.google.com/maps/contrib/104819497579982636149\"\u003eShawn Lynch\u003c/a\u003e"
+               ],
+               "photo_reference" : "CmRdAAAAlmlCvSI-EEK4qHov0hoD1mlpM51AQ43zBrHeSYfGsx30Nm4auySWaAoDWy-cU6dHbEST9F4KLNr4RFfr3aZ8nGoR5B39s2IQUZQ8rABjWX8Stct6d-GUDVm2BfxpAIjsEhBSZtt2hreX9FlV1PVVTzhZGhS2h7rRaXXWVrpEN7fr3_4xc0iwaQ",
+               "width" : 1365
+            }
+         ],
+         "place_id" : "ChIJ_7SGmp2NWYgR_DxPJGayqME",
+         "reference" : "CoQBeAAAACfTDUvB0C3N-SND56nSISfmJ94Scu4ihlk4QgJ2TATvcvUjp-VTBjUneS_2s9CkBbMzU0S3GEOu3iFocj0rFCSelquifReVYlVnLShZvf7KaPvrgFAwdoHPCU5DuHfHlTjr5F0JtUKeHOmJueEiGWWT_H5dmB1xwCLB9Un82jZ8EhDsU-4QJXnEdjtzkLycABE8GhTvkArKseM-v5ausB5xXYvZslPAEA",
+         "scope" : "GOOGLE",
+         "types" : [ "real_estate_agency", "point_of_interest", "establishment" ],
+         "vicinity" : "673 Brevard Road, Asheville"
+      },
+      {
+         "geometry" : {
+            "location" : {
+               "lat" : 35.543821,
+               "lng" : -82.597525
+            }
+         },
+         "icon" : "https://maps.gstatic.com/mapfiles/place_api/icons/generic_business-71.png",
+         "id" : "e2ee61b8330da0491907b7748f78e6f80614dd37",
+         "name" : "Re/Max All Stars Realty: Toby Davis",
+         "place_id" : "ChIJ98u0Z8uPWYgRHcgoA1Xcn7o",
+         "reference" : "CoQBdwAAANXuBZyY8o-XhpN-i0AfT9EETdzUuj3asur-eJQmHxgr4eDKEYe-yVm-kNehv9xiZJSTDFbUqYBpxU5ZNFrpK4xOBK5lP388YI2TP1_Ixzsqxc0bTeo5wc1nnN8Fib1HPuvL8yRfflRyGtgvtd0xUhJTeqdu6RLmeg0aISJ-SUhqEhCCuWycXI4SPPh-hIG3GaS_GhTgtSt1FUrtUaocv85jU7sEp32bSw",
+         "scope" : "GOOGLE",
+         "types" : [ "real_estate_agency", "point_of_interest", "establishment" ],
+         "vicinity" : "673 Brevard Road, Asheville"
+      },
+      {
+         "geometry" : {
+            "location" : {
+               "lat" : 35.543821,
+               "lng" : -82.597525
+            }
+         },
+         "icon" : "https://maps.gstatic.com/mapfiles/place_api/icons/generic_business-71.png",
+         "id" : "d564cdf8e27aa9ff9b00a2e92fb8d300b189388a",
+         "name" : "Remax: Judy Dawkins",
+         "place_id" : "ChIJV2oXAw-NWYgRvqGlfj7Au_c",
+         "reference" : "CnRnAAAAasoZma_kt3OaCNRu0eWT_FZrEQyuPKs2rkIuDBoAaAOrxKsy9MZvgDHkYU_iV4U96SI2bhGfVvTYDhMDVG1Icfini_FXtgQdCokGOuqKUgsoDshgIR5vgXKlhYx8IaWSJqBiiO17gs0Hp0tJrYgdKRIQpf0g43405kxI-jT7MdwnxRoUdRKXuR-BP8tfMxPA-5U3lwNswXI",
+         "scope" : "GOOGLE",
+         "types" : [ "real_estate_agency", "point_of_interest", "establishment" ],
+         "vicinity" : "673 Brevard Road, Asheville"
+      }
+   ]
--- js/script.js
+++ js/script.js
@@ -1,72 +1,14 @@
-//using knockout.js to bind values
-mapMarker = function (name, address, city, lat, lng) {
-  this.name = ko.observable(name);
-  this.address = ko.observable(address);
-  this.city = ko.observable(city);
-  this.lat = ko.observable(lat);
-  this.lng = ko.observable(lng);
+var mapMarker;
+var markerPosition;
 
-  //defines how markers are displayed locations for each realtor
-//function createMarker(){
-  var marker = new google.maps.Marker({
-      position: new google.maps.LatLng(lat, lng),
-      map: map,
-      draggable: false,
-      animation:google.maps.Animation.DROP,
-      icon: 'images/mapmarker_green.png'
-  });
-
-  //creates info window shown on mouse event
-  var infowindowcontent = '<div>' + name + '</div>' + '<div>' + address + '</div>' + '<div>' + city + '</div>';
-  var infowindow = new google.maps.InfoWindow({
-    content: infowindowcontent
-    });
-
-    //adds listener for realtor details on mouseover and mouseout
-    google.maps.event.addListener(marker, 'mouseover', function() {
-      infowindow.open(map,marker);
-      });
-    google.maps.event.addListener(marker, 'mouseout', function() {
-      infowindow.close(map,marker);
-      });
-
-    var markerPostion;
-    //for list click event reflected on map
-    self.logClick = (function(markerPostion) {
-      return function() {
-        markerPostion = new google.maps.LatLng(this.lat, this.lng);
-        marker.setPosition(markerPostion);
-        marker.setAnimation(google.maps.Animation.BOUNCE);
-        setTimeout(function(){ marker.setAnimation(null); }, 1500);
-        marker.setIcon("images/mapmarker_purple.png");
-      };
-  })(markerPostion);
-}
-    //for filtering list with search box
-    var viewModel = {
-        query: ko.observable('')
-    };
-
-    //creates list of realtors for list box
-    viewModel.realtorList = ko.dependentObservable(function() {
-        var search = this.query().toLowerCase();
-        return ko.utils.arrayFilter(realtorList, function(realtor) {
-            return realtor.name.toLowerCase().indexOf(search) >= 0;
-        });
-    }, viewModel);
-
-
-      self.logClick = (function() {
-
-      });
-
-
+var MapViewModel = function() {
     loadMap = function(items){
+
         //creates map for initial display using settings below when page is loaded
         var myOptions = {
             zoom: 12,
-            center: new google.maps.LatLng(35.598888, -82.551392),
-            mapTypeId: 'terrain'
+            center: new google.maps.LatLng(35.559139, -82.567327),
+            mapTypeId: 'roadmap'
         };
         map = new google.maps.Map($('#map')[0], myOptions);
 
@@ -74,30 +16,104 @@ mapMarker = function (name, address, city, lat, lng) {
         self.list = ko.observableArray([]);
 
         //iterates through realtor data for displaying map markers
-        realtorList.forEach(function(realtor) {
+        results.forEach(function(realtor) {
             self.list.push( new mapMarker(realtor) );
+            items = results;
 
-            items = realtorList ;
+        $("li").hover(function() {
+            $("li").css("cursor", "pointer");
+            }, function() {
+        $("li").css("cursor", "auto");
+            });
         });
-            //populates array of realtors data info for map markers info window on mouse event
+
+            //populates array of realtor and data info for map markers info window on mouse event
             for (var j = 0; j < items.length; j++) {
-              self.list.push( new mapMarker( items[j]['name'], items[j]['address'], items[j]['city'], items[j]['lat'], items[j]['lng']));
-        }
+              self.list.push( new mapMarker( items[j]['name'], items[j]['vicinity'], items[j]['geometry']['location']['lat'], items[j]['geometry']['location']['lng']));
+            }
+      };
+
+
+
+//*****************************************
+    //using knockout.js to bind values
+      //**************************************
+
+    mapMarker = function (name, vicinity, lat, lng) {
+      this.name = ko.observable(name);
+      this.vicinity = ko.observable(vicinity);
+      this.lat = ko.observable(lat);
+      this.lng = ko.observable(lng);
+      //defines how markers are displayed locations for each realtor
+    //function createMarker(){
+      var marker = new google.maps.Marker({
+          position: new google.maps.LatLng(lat, lng),
+          map: map,
+          draggable: false,
+          icon: 'images/mapmarker_green.png',
+          animation:google.maps.Animation.DROP
+      });
+
+                    //console.log(map,marker);
+    //creates info window shown on mouse event
+    var infowindowcontent = '<div>' + name + '</div>' + '<div>' + vicinity + '</div>';
+    var infowindow = new google.maps.InfoWindow({
+      content: infowindowcontent
+      });
+
+      //adds listener for realtor details on mouseover and mouseout
+      google.maps.event.addListener(marker, 'mouseover', function() {
+        infowindow.open(map,marker);
+        });
+
+      google.maps.event.addListener(marker, 'mouseout', function() {
+        infowindow.close(map,marker);
+        });
+
+      //google.maps.event.addListener(marker, 'mouseover', function() {
+        //marker.setMap(null);
+      //});
+
+      //for list click event reflected on map
+      self.logClick = (function(markerPosition) {
+        return function() {
+          marker.setIcon("images/mapmarker_purple.png");
+          markerPosition = new google.maps.LatLng(this.geometry.location.lat, this.geometry.location.lng);
+          marker.setPosition(markerPosition);
+          marker.setAnimation(google.maps.Animation.BOUNCE);
+          setTimeout(function(){ marker.setAnimation(null); }, 1200);
+        };
+    })(markerPosition);
+  }
+//creates list of realtors for list box
+    viewModel.results = ko.dependentObservable(function() {
+        var search = this.query().toLowerCase();
+        return ko.utils.arrayFilter(results, function(realtor) {
+          if(realtor.name.toLowerCase().indexOf(search) >= 0){
+            return realtor.name.toLowerCase().indexOf(search) >= 0;
+          }
+        });
+    },
+    viewModel);
+
+}
+
+    //for filtering list with search box
+    var viewModel = {
+        results: ko.observableArray([]),
+        query: ko.observable('')
     };
 
+  self.logClick = (function() {});
+
 $(document).ready(function () {
-    ko.applyBindings(viewModel);
+    MapViewModel();
+    var applied = false;
+    if (!applied) {
+        ko.applyBindings(viewModel);
+        applied = true;
+    }
     loadMap();
 });
-    //var reviewUrl = "http://www.zillow.com/webservice/ProReviews.htm?zws-id=X1-ZWz1a2tdkm61vv_4kciw&screenname=Lorraine%20Silverman%201&callback=getJSON";
 
-/*
-    $.getJSON(reviewUrl, function (data) {
-        alert('success = ' + reviewUrl);
-        if (data.results.length > 0) {
-    alert(data.results.length);
-        } else {
-            alert('No match found for information specified.');
-        }
-    });
-*/
+
